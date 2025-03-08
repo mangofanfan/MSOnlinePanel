1277d970a6d099b25fce34d085d3b8b336af73a8
MangoFanFan_ <mangofanfanw@icloud.com>
2025-03-08 15:01:36 +0800

    上传文件（图像）优化

--- Upload/forms.py
+++ Upload/forms.py
@@ -4,4 +4,9 @@ from django import forms
 class UploadForm(forms.Form):
     file = forms.FileField(label="上传文件",
                            required=True,
-                           help_text="在此处选择并上传文件")
+                           help_text="在此处选择并上传文件",
+                           widget=forms.FileInput(attrs={'class': 'form-control'}))
+    filename = forms.CharField(label="文件名",
+                               required=True,
+                               help_text="您可以手动指定一个文件名",
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
